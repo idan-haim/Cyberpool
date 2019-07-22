8d528d27b1afaa436654f9433c0973a58d4ba8fb
Idan Haim <idan@cybereason.com>
2019-07-22 14:29:56 +0300

    Refactoring SimpleHTTPRequestHandler to BaseHTTPRequestHandler

--- carpool_server.py
+++ carpool_server.py
@@ -1,27 +1,49 @@
-# import http.server
-# import socketserver
-#
-# PORT = 8001
-#
-# Handler = http.server.SimpleHTTPRequestHandler
-#
-# with socketserver.TCPServer(("", PORT), Handler) as httpd:
-#     print("serving at port", PORT)
-#     httpd.serve_forever()
+import json
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
-PORT = 8001
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+class S(BaseHTTPRequestHandler):
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
     def do_GET(self):
+        self._set_headers()
+        self.wfile.write(b"Hi<br/> Sharing is Caring")
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        self._set_headers()
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello, world!')
+
+        slack_data = json.loads(self.data_string)
+        with open("test.json", "w") as outfile:
+            json.dump(slack_data, outfile)
+        print("{}".format(slack_data))
+        self.wfile.write(b'POST!!!!!!!!!!!!!!!')
+
+        return
 
 
-httpd = HTTPServer(('localhost', PORT), SimpleHTTPRequestHandler)
-print("serving at port", PORT)
-httpd.serve_forever()
+def run(server_class=HTTPServer, handler_class=S, port=8001):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}')
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
