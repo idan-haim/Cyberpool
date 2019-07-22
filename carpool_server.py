b19afe6a1c129fd3eba2d0a585a93708c5472240
Noy Maday <Noy.Maday@cyberdomain.local>
2019-07-22 22:17:37 +0300

    with errors

--- carpool_server.py
+++ carpool_server.py
@@ -2,6 +2,7 @@ import json
 import logging
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib import request, parse
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -32,26 +33,46 @@ class S(BaseHTTPRequestHandler):
         self.end_headers()
 
         self.data_bytes = self.rfile.read(int(self.headers['Content-Length']))
-        slack_json = json.loads(self.data_bytes)
+        my_json = self.data_bytes.decode('utf8').replace("'", '"')
+        slack_json = json.loads(my_json)
         logger.info(f'Get a new salsh command from user: {slack_json}')
-        data_to_mysql = self.to_json(slack_json)
+        data_to_mysql = self.parse_data_to_sql(slack_json)
         # print the parsed json to the screen
         self.wfile.write(json.dumps(data_to_mysql, indent=2).encode('utf-8'))
         return
 
-    def to_json(self, slack_input):
-        text = slack_input["text"]
-        text = text.split(',')
-        user_id = slack_input["slack_id"]
-        if text[0] == 'create':
-            res = {"roll": "0", "from": text[1].strip(), "to": text[2].strip(), "date": text[3].strip(),
-                   "time": text[4].strip(), "seats": text[5].strip(), "id": user_id}
+    def parse_data_to_sql(self, data):
+        text = data.get("text")
+        if not text:
+            response_url = data["response_url"]
+            self.send_error_to_slack(response_url[0], "There is no text")
+        if len(text) < 6:
+            response_url = data["response_url"]
+            self.send_error_to_slack(response_url[0], "There is not enough arguments")
         else:
-            res = {"roll": "1", "from": text[1].strip(), "to": text[2].strip(), "date": text[3].strip(),
-                   "time": text[4].strip(), "id": user_id}
-        logger.info(f'The json for the database {res}')
-        return res
+            text = text.split(',')
+            user_id = data["channel_id"]
+            if text[0] == 'create':
+                res = {"roll": "0", "from": text[1].strip(), "to": text[2].strip(), "date": text[3].strip(),
+                       "time": text[4].strip(), "seats": text[5].strip(), "id": user_id}
+            else:
+                res = {"roll": "1", "from": text[1].strip(), "to": text[2].strip(), "date": text[3].strip(),
+                       "time": text[4].strip(), "id": user_id}
+            logger.info(f'The json for the database {res}')
+            print(res)
+            return res
 
+    def send_error_to_slack(self, response_url, msg):
+        # post = {"text": "{0}".format(msg)}
+        # try:
+        #     json_data = json.dumps(post)
+        #     req = request.Request(response_url,
+        #                           data=json_data.encode('ascii'),
+        #                           headers={'Content-Type': 'application/json'})
+        #     request.urlopen(req)
+        # except Exception as em:
+        #     print("EXCEPTION: " + str(em))
+        print("wrong input")
 
 def run(server_class=HTTPServer, handler_class=S, port=8001):
     server_address = ('', port)
