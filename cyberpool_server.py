56d2cf55643987afe257ace62b0236b8cd04e273
Idan Haim <idan@cybereason.com>
2019-07-22 22:15:01 +0300

    Adding convert_query_string_to_dict

--- cyberpool_server.py
+++ cyberpool_server.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import urllib
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
@@ -32,13 +33,21 @@ class S(BaseHTTPRequestHandler):
         self.end_headers()
 
         self.data_bytes = self.rfile.read(int(self.headers['Content-Length']))
-        slack_json = json.loads(self.data_bytes)
-        logger.info(f'Get a new salsh command from user: {slack_json}')
+        logger.info(f'Got new raw request: {self.data_bytes}')
+        data_dict = self.convert_query_string_to_dict(self.data_bytes)
+        logger.info(f'data_dict: {data_dict}')
+        slack_json = json.loads(str(data_dict))
+        logger.info(f'Request transformed to json: {slack_json}')
         data_to_mysql = self.to_json(slack_json)
         # print the parsed json to the screen
         self.wfile.write(json.dumps(data_to_mysql, indent=2).encode('utf-8'))
-        
+
         return
+
+    def convert_query_string_to_dict(self, data):
+        data_str = data.decode('utf-8')
+        qs_data = urllib.parse.urlsplit(data_str).path.replace("'", '"')
+        return json.loads(qs_data)
 
     def to_json(self, slack_input):
         text = slack_input["text"]
